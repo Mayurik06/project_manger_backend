856e08fa130276081fbebd72fa7640bbf8a4be36
Mayuri <khapnemayuri48@gmail.com>
2024-10-03 15:46:31 +0530

    checking cors error

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:process.env.FRONTEND_URL || "project-management-system-nine-sooty.vercel.app",
+    origin:'*',
     methods:['GET','POST','PATCH','DELETE'],
     credentials:true
 }))
