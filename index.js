2e060f93174d079ced3192e0a933aa2b68d5b37c
Mayuri <khapnemayuri48@gmail.com>
2024-10-03 15:53:15 +0530

    solved cors issue

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:'*',
+    origin:process.env.FRONTEND_URL,
     methods:['GET','POST','PATCH','DELETE'],
     credentials:true
 }))
