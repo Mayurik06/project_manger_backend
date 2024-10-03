e44eff1d858110fb6c02e15268e1fc5a1452969e
Mayuri <khapnemayuri48@gmail.com>
2024-10-03 15:04:55 +0530

    change the cor config.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:process.env.FRONTEND_URL || "project-management-system-nine-sooty.vercel.app",
     methods:['GET','POST','PATCH','DELETE'],
     credentials:true
 }))
